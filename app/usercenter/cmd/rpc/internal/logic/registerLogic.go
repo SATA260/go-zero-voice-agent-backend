7ae7f5f29b5aa45c72cc0672f6272cacf093776d
root <root@LGsata.localdomain>
2025-10-18 14:12:17 +0800

    feat:创建goctl模板，初始化usercenter的model

---
+++ app/usercenter/cmd/rpc/internal/logic/registerLogic.go
@@ -0,0 +1,29 @@
+package logic
+
+import (
+	"context"
+
+	"go-zero-voice-agent/app/usercenter/cmd/rpc/internal/svc"
+	"go-zero-voice-agent/app/usercenter/cmd/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *RegisterLogic) Register(in *pb.RegisterReq) (*pb.RegisterResp, error) {
+
+	return &pb.RegisterResp{}, nil
+}
