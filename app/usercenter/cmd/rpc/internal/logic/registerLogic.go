6c34baf186a3a00900868e8aa37dcdc50bc59f74
root <root@LGsata.localdomain>
2025-10-20 15:03:40 +0800

    feat: 完成邮箱注册逻辑

--- app/usercenter/cmd/rpc/internal/logic/registerLogic.go
+++ app/usercenter/cmd/rpc/internal/logic/registerLogic.go
@@ -3,13 +3,17 @@ package logic
 import (
 	"context"
 
+	"go-zero-voice-agent/app/usercenter/cmd/rpc/internal/consts"
 	"go-zero-voice-agent/app/usercenter/cmd/rpc/internal/svc"
 	"go-zero-voice-agent/app/usercenter/cmd/rpc/pb"
+	"go-zero-voice-agent/app/usercenter/cmd/rpc/usercenter"
 	"go-zero-voice-agent/app/usercenter/model"
+	"go-zero-voice-agent/pkg/tool"
 	"go-zero-voice-agent/pkg/xerr"
 
 	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/logx"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 var ErrUserAlreadyRegisterError = xerr.NewErrMsg("user has been registered")
@@ -36,7 +40,62 @@ func (l *RegisterLogic) Register(in *pb.RegisterReq) (*pb.RegisterResp, error) {
 	if user != nil {
 		return nil, errors.Wrapf(ErrUserAlreadyRegisterError, "Register user exists email:%s, err:%v", in.Email, err)
 	}
-	
 
-	return &pb.RegisterResp{}, nil
+	// 验证验证码
+	verifyCodeKey := consts.GetRegisterVerifyCodeCacheKey(in.Email)
+	cachedCode, err := l.svcCtx.RedisClient.Get(verifyCodeKey)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "get verify code from redis err:%v", err)
+	}
+	if cachedCode != in.VerifyCode {
+		return nil, errors.Wrapf(xerr.NewErrMsg("verify code error"), "email:%s, input code:%s, cache code:%s", in.Email, in.VerifyCode, cachedCode)
+	}
+
+	// 创建用户
+	var userId int64
+	if err := l.svcCtx.UserModel.Trans(l.ctx, func(context context.Context, session sqlx.Session) error {
+		user := new(model.User)
+		user.Email = in.Email
+		if len(in.Nickname) == 0 {
+			user.Nickname = tool.Krand(8, tool.KC_RAND_KIND_ALL)
+		}
+		if len(in.Password) > 0 {
+			user.Password = tool.MD5HashWithSalt(in.Password, model.UserPassWordSalt)
+		}
+
+		insertResult, err := l.svcCtx.UserModel.Insert(context, session, user)
+		if err != nil {
+			return errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "Register db user Insert err:%v,user:%+v", err, user)
+		}
+		lastId, err := insertResult.LastInsertId()
+		if err != nil {
+			return errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "Register db user insertResult.LastInsertId err:%v,user:%+v", err, user)
+		}
+		userId = lastId
+
+		userAuth := new(model.UserAuth)
+		userAuth.UserId = lastId
+		userAuth.AuthKey = in.AuthKey
+		userAuth.AuthType = in.AuthType
+		if _, err := l.svcCtx.UserAuthModel.Insert(context, session, userAuth); err != nil {
+			return errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "Register db user_auth Insert err:%v,userAuth:%v", err, userAuth)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	generateTokenLogic := NewGenerateTokenLogic(l.ctx, l.svcCtx)
+	tokenResp, err := generateTokenLogic.GenerateToken(&usercenter.GenerateTokenReq{
+		UserId: userId,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(ErrGenerateTokenError, "GenerateToken userId : %d", userId)
+	}
+
+	return &usercenter.RegisterResp{
+		AccessToken:  tokenResp.AccessToken,
+		AccessExpire: tokenResp.AccessExpire,
+		RefreshAfter: tokenResp.RefreshAfter,
+	}, nil
 }
